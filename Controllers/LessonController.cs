2e4ec751498356d941e95e7df5d14d37f30e5055
linhtruong2511 <linhtk251104@gmail.com>
2026-01-08 13:36:59 +0700

    lesson update

---
+++ Controllers/LessonController.cs
@@ -0,0 +1,50 @@
+﻿using System.Threading.Tasks;
+using EnglishCenter.DTO;
+using EnglishCenter.Models;
+using EnglishCenter.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EnglishCenter.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LessonController : ControllerBase
+    {
+        ILessonService lessonService;
+        public LessonController(ILessonService lessonService)
+        {
+            this.lessonService = lessonService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Lesson>>> GetAll(int sectionId)
+        {
+            var lessons = await lessonService.GetLessonsAsync(sectionId);
+            return Ok(lessons);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Lesson>> Get(int id)
+        {
+            return await lessonService.GetLessonAsync(id);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Lesson>> Post([FromForm] LessonCreateDto dto)
+        {
+            return Ok(await lessonService.AddLessonAsync(dto));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Lesson>> Put(int id, [FromForm] LessonUpdateDto dto)
+        {
+            return Ok(await lessonService.UpdateLessonAsync(id, dto));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task Delete(int id)
+        {
+            await lessonService.DeleteLessonAsync(id);
+        }
+    }
+}
